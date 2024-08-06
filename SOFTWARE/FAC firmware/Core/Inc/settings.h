e3f5a366c158f9089fc1206db5d31dc0548bd57b
FloppyO1 <thefloppylab@gmail.com>
2024-08-06 13:54:39 +0200

    first commit

---
+++ SOFTWARE/FAC firmware/Core/Inc/settings.h
@@ -0,0 +1,75 @@
+/*
+ * settings.h
+ *
+ *  Created on: Jul 14, 2024
+ *      Author: Filippo Castellan
+ */
+
+#ifndef INC_SETTINGS_H_
+#define INC_SETTINGS_H_
+
+#include "stm32f0xx_hal.h"
+
+#define EEPROM_DATA_BEGIN_ADDRESS 0	// data address
+#define EEPROM_ISFIRSTBOOT_ADDRESS 100	// isfirstboot data address
+
+/* ------------- COMMANDs LIST ------------- */
+#define COMMAND_TYPE_SEPARATOR ">"
+//CHANNELs MAPPING
+// TYPE (VALUE = number of the channel)
+#define COMMAND_TH2CH "TH2CH"	// throttel to channel..
+#define COMMAND_ST2CH "SH2CH"	// steering
+#define COMMAND_WP2CH "WP2CH"	// weapon
+#define COMMAND_S12CH "S12CH"	// servo 1 (HV)
+#define COMMAND_S22CH "S22CH"	// servo 2
+#define COMMAND_ARM "ARM2CH"		// arm robot
+// DC MOTORs/ SERVOs REVERSE
+// TYPE (VALUE = 0 if no reverse, 1 if reverse)
+#define COMMAND_M1REV "M1REV"	// reverse the rotation of the motor
+#define COMMAND_M2REV "M2REV"
+#define COMMAND_M3REV "M3REV"
+#define COMMAND_S1REV "S1REV"	// reverse the position of the servo
+#define COMMAND_S2REV "S2REV"
+// Mx ASSIGNMENT
+// TYPE
+#define COMMAND_M1ASS "M1ASS"	// assign the motor 1 to MOTOR LEFT, MOTOR, RIGHT, WEAPPON MOTOR
+#define COMMAND_M2ASS "M2ASS"
+#define COMMAND_M3ASS "M3ASS"
+// VALUE
+#define VALUE_ML "ML"
+#define VALUE_MR "MR"
+#define VALUE_WP "WP"
+// WEAPPON DOUBLE/SINGLE DIRECTION
+// TYPE (VALUE = 1 if double direction, 0 if single direction)
+#define COMMAND_WPDD "WPDD"
+// TANK MODE
+// TYPE (VALUE = 0 if disabled, 1 if enabled)
+#define COMMAND_TANKON "TNKON"
+// OTHER
+// TYPE
+#define COMMAND_NODISARM "NODISARM" // VALUE = 0 if disabled, 1 if enabled
+#define COMMAND_LIMITVOLTAGE "VLIM"	// VALUE = voltage*10 (3.3V -> 33)
+#define COMMAND_CUTOFFVOLTAGE "VCUTOFF" // VALUE = voltage*10 (3.3V -> 33)
+#define COMMAND_RXPWMMODE "RXPWM"	// 1 if the reciever output a pwm signal, else PPM
+// SAVE/READ SETTINGS
+// TYPE (value can be any number)
+#define COMMAND_SAVE "SAVE"
+#define COMMAND_READ "READ"
+
+typedef struct Command {
+	char type[10];
+	char value[10];
+} Command;
+
+Command decodeCommand(char *buffer);
+
+void serialPrintString(uint8_t *s);
+int modifySettingsWithCommand();
+void loadSettingsFromEeprom();
+void storeSettingsToEeprom();
+void readSettingsFromEeprom();
+uint8_t read_byte_eeprom(uint8_t address);
+void write_byte_eeprom(uint8_t address, uint8_t value);
+void serialPrintSettings();
+
+#endif /* INC_SETTINGS_H_ */
---
+++ SOFTWARE/FAC firmware/Core/Src/servo.c
@@ -0,0 +1,59 @@
+/*
+ * servo.c
+ *
+ *  Created on: Jul 7, 2024
+ *      Author: Filippo Castellan
+ */
+
+#include "servo.h"
+
+extern uint8_t s1Rev;
+extern uint8_t s2Rev;
+
+void initServos(TIM_HandleTypeDef *htim) {
+	TIM16->CCR1 = S1_MIN_TIME_PWM;
+	TIM17->CCR1 = S2_MIN_TIME_PWM;
+	HAL_TIMEx_PWMN_Start(htim, TIM_CHANNEL_1);
+}
+
+uint8_t getServoRev(uint8_t servoN) {	// return 1 if the motorN is reversed, else 0
+	uint8_t isRev = 0;
+	switch (servoN) {
+		case S1:
+			isRev = s1Rev;
+			break;
+		case S2:
+			isRev = s2Rev;
+			break;
+	}
+	return isRev;
+}
+
+
+/*
+ * to calculate the factor, use this formula:
+ * P = 1/FREQ
+ * P(us) = P * 1'000'000
+ * factor = 10000/P(us)
+ * if factor < 1 divide by the new factor
+ * new factor = 1/factor
+ */
+void setServoAngle(uint8_t servoN, uint8_t percentage) {
+	const uint8_t factor = 2;
+	uint16_t temp = percentage;
+	if (temp > 100) temp = 100;
+	if (temp < 0) temp = 0;
+
+	if (getServoRev(servoN)) temp = 100 - temp;	// take in count if the servo must be reversed or not
+
+	switch (servoN) {
+		case S1:
+			temp = map(temp, 0, 100, S1_MIN_TIME_PWM, S1_MAX_TIME_PWM);
+			TIM16->CCR1 = temp * factor;
+			break;
+		case S2:
+			temp = map(temp, 0, 100, S2_MIN_TIME_PWM, S2_MAX_TIME_PWM);
+			TIM17->CCR1 = temp * factor;
+			break;
+	}
+}
