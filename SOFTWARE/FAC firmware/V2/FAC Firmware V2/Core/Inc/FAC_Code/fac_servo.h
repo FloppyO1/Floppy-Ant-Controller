491a7057d62d4b130c62a47e1b33d45d3137a0bc
FloppyO1 <thefloppylab@gmail.com>
2025-08-04 19:06:15 +0200

    updated servo.c and .h

--- SOFTWARE/FAC firmware/V2/FAC Firmware V2/Core/Inc/FAC_Code/fac_servo.h
+++ SOFTWARE/FAC firmware/V2/FAC Firmware V2/Core/Inc/FAC_Code/fac_servo.h
@@ -12,11 +12,17 @@
 #include "FAC_Code/config.h"
 
 #define SERVOS_NUMBER 2
+#define MAX_SERVO_VALUE RECEIVER_CHANNEL_RESOLUTION-1
 
 typedef struct Servo {
 	uint16_t position;		// rage from 0 to max vale
 	uint8_t is_enable;		// TRUE /FALSE
 	uint8_t is_reversed;	// TRUE/FALSE, if TRUE the motor will spin in the inverse direction of dir
+	uint16_t servo_freq;	// will be the same for every servo, it will sincronyzed on the servo set freq.
+	uint16_t tim_channel;	// channel of timer used
 } Servo;
 
+uint16_t FAC_servo_GET_position(uint8_t servoNumber);
+uint8_t FAC_servo_GET_is_enable(uint8_t servoNumber);
+uint8_t FAC_servo_GET_is_reversed(uint8_t servoNumber);
 #endif /* INC_FAC_CODE_FAC_SERVO_H_ */
